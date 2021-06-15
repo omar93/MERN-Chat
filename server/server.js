29508d81451da897d6397856bb709345b5da6cf9
Omar Alhamad <omar93@hotmail.se>
2021-06-15 17:42:25 +0200

    inital commit, folder in place, express in place, time to get the backend working

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 4500
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(port, _ => console.log(`Server running at http://localhost:${port}`))
